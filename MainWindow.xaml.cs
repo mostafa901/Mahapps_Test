fb604cd3728578147fc9391027dc63e2b47e9be1
mostafa90 <mostafa90@gmail.com>
2016-12-22 23:12:15 +0400

    test

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IconPacks_Vs_mahapps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           Loaded+= delegate { testmessage(); };
+            
+        }
+
+        public async Task<bool> testmessage()
+        {
+          await  this.ShowMessageAsync("test Title", "test Message", MessageDialogStyle.AffirmativeAndNegative, new MetroDialogSettings()
+          {
+               AffirmativeButtonText="OK",
+               ColorScheme = MetroDialogColorScheme.Theme
+          });
+            return true;
+        } 
+    }
+}
---
+++ MetroWin_Lib/Class_Windows.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace MetroWin_Lib
+{
+    /// <summary>
+    /// Interaction logic for Class_Windows.xaml
+    /// </summary>
+    public partial class Class_Windows : MetroWindow
+    {
+        public Class_Windows()
+        {
+            InitializeComponent();
+            Loaded += delegate { testmessage(); };
+
+        }
+
+        public async Task<bool> testmessage()
+        {
+            await this.ShowMessageAsync("test Title", "test Message", MessageDialogStyle.AffirmativeAndNegative, new MetroDialogSettings()
+            {
+                AffirmativeButtonText = "OK",
+                ColorScheme = MetroDialogColorScheme.Theme
+            });
+            return true;
+        }
+    }
+}
