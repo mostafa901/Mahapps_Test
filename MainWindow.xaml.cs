ea2ec70bdc4e1ad2e6d8a9030d2d2f35317ebe7a
Moustafa Khalil <mostafa90@gmail.com>
2019-09-06 00:59:33 +0400

    base

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,82 +21,80 @@ using System.Windows.Shapes;
 
 namespace Test_Repo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : MetroWindow
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : MetroWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
 
-            var uc_base = new uc_BaseControl();
-            // MainGrid.Children.Add(uc_base);
-            // Grid.SetRow(uc_base, 1);
+			var uc_base = new uc_BaseControl();
+			// MainGrid.Children.Add(uc_base);
+			// Grid.SetRow(uc_base, 1);
 
-            Content = uc_base;
-            PreviewMouseDoubleClick+= delegate
-            {
-               uc_base. flout.IsOpen = !uc_base.flout.IsOpen;
-            };
+			Content = uc_base;
+			PreviewMouseDoubleClick += delegate
+			 {
+				 uc_base.flout.IsOpen = !uc_base.flout.IsOpen;
+			 };
 
-
-
-            DataContext = new mc_test(); ;
-
-            
-          
-        }
-
-        
-
-       async private void Test_Click(object sender, RoutedEventArgs e)
-        {
-           var prg =  await this.ShowProgressAsync($"Window Size Mode is:  {this.SizeToContent.ToString()}", "", true);
-            prg.Canceled += async delegate
-            {
-                await prg.CloseAsync();
-            };
-        }
-    }
-
-    public class mc_test : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
+			DataContext = new mc_test(); ;
 
 
-        public mc_test()
-        {
-            Items = new ObservableCollection<string>();
-            Items.Add("123");
-            Items.Add("ddas");
-            Items.Add("asda");
-            Items.Add("12adas3");
-        }
-        #region Items
 
-        private ObservableCollection<string> _Items;
-
-        public ObservableCollection<string> Items
-        {
-            get
-            {
-                return _Items;
-            }
-            set {  _Items= value;Notify(nameof(Items)); }
-
-        }
-        #endregion
+		}
 
 
-        public void Notify(string prop)
-        {
-            if (PropertyChanged != null)
-            {
+
+		async private void Test_Click(object sender, RoutedEventArgs e)
+		{
+			var prg = await this.ShowProgressAsync($"Window Size Mode is:  {this.SizeToContent.ToString()}", "", true);
+			prg.Canceled += async delegate
+			{
+				await prg.CloseAsync();
+			};
+		}
+	}
+
+	public class mc_test : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
 
 
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
-            }
-        }
-    }
+		public mc_test()
+		{
+			Items = new ObservableCollection<string>();
+			Items.Add("123");
+			Items.Add("ddas");
+			Items.Add("asda");
+			Items.Add("12adas3");
+		}
+		#region Items
+
+		private ObservableCollection<string> _Items;
+
+		public ObservableCollection<string> Items
+		{
+			get
+			{
+				return _Items;
+			}
+			set { _Items = value; Notify(nameof(Items)); }
+
+		}
+		#endregion
+
+
+		public void Notify(string prop)
+		{
+			if (PropertyChanged != null)
+			{
+
+
+				PropertyChanged(this, new PropertyChangedEventArgs(prop));
+			}
+		}
+	}
 }
