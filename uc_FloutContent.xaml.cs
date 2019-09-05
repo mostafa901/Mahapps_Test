a0a1cbda91fce3aba77a7f1850919c40262ee519
Moustafa Khalil <mostafa90@gmail.com>
2019-09-06 01:29:43 +0400

    formating

--- uc_FloutContent.xaml.cs
+++ uc_FloutContent.xaml.cs
@@ -1,28 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace Test_Repo
 {
-    /// <summary>
-    /// Interaction logic for uc_FloutContent.xaml
-    /// </summary>
-    public partial class uc_FloutContent : UserControl
-    {
-        public uc_FloutContent()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for uc_FloutContent.xaml
+	/// </summary>
+	public partial class uc_FloutContent : UserControl
+	{
+		public uc_FloutContent()
+		{
+			InitializeComponent();
+		}
+	}
 }
