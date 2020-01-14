115008aa2c62f7c7e38a8329288282a7bb5c3981
Moustafa Khalil <mostafa90@gmail.com>
2020-01-14 09:56:40 +0400

    updates,, still not working with latest version of mahaaps :(

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,94 +11,90 @@ using System.Windows.Threading;
 
 namespace Test_Repo
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow: MetroWindow
-	{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow: MetroWindow
+    {
+        Dispatcher mydisp = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = mv = new ViewModel(DialogCoordinator.Instance);
+            mydisp = Dispatcher.CurrentDispatcher;
+        }
 
-		public MainWindow()
-		{
-			InitializeComponent();
-			DataContext = mv = new ViewModel(DialogCoordinator.Instance);
-		}
+        public ViewModel mv { get; private set; }
 
-		public ViewModel mv { get; private set; }
+        async private void Test_Click(object sender, RoutedEventArgs e)
+        {
+            await mv.StartNewThread(mydisp);
+        }
+    }
 
-		async private void Test_Click(object sender, RoutedEventArgs e)
-		{
-			await mv.StartNewThread();
-		}
-	}
+    public class mc_test: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
 
-	public class mc_test: INotifyPropertyChanged
-	{
-		public event PropertyChangedEventHandler PropertyChanged;
+        public mc_test()
+        {
+            Items = new ObservableCollection<string>();
+            Items.Add("123");
+            Items.Add("ddas");
+            Items.Add("asda");
+            Items.Add("12adas3");
+        }
 
-		public mc_test()
-		{
-			Items = new ObservableCollection<string>();
-			Items.Add("123");
-			Items.Add("ddas");
-			Items.Add("asda");
-			Items.Add("12adas3");
-		}
+        #region Items
 
-		#region Items
+        private ObservableCollection<string> _Items;
 
-		private ObservableCollection<string> _Items;
+        public ObservableCollection<string> Items
+        {
+            get
+            {
+                return _Items;
+            }
+            set { _Items = value; Notify(nameof(Items)); }
+        }
 
-		public ObservableCollection<string> Items
-		{
-			get
-			{
-				return _Items;
-			}
-			set { _Items = value; Notify(nameof(Items)); }
-		}
+        #endregion Items
 
-		#endregion Items
+        public void Notify(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
 
-		public void Notify(string prop)
-		{
-			if (PropertyChanged != null)
-			{
-				PropertyChanged(this, new PropertyChangedEventArgs(prop));
-			}
-		}
-	}
+    public class ViewModel
+    {
+        private IDialogCoordinator instance;
 
-	public class ViewModel
-	{
-		private IDialogCoordinator instance;
+        public ViewModel(IDialogCoordinator instance)
+        {
+            this.instance = instance;
+        }
 
-		public ViewModel(IDialogCoordinator instance)
-		{
-			this.instance = instance;
-		}
+        async public Task StartNewThread(Dispatcher mydisp)
+        {
+             
+            var thread = new Thread(new ThreadStart(() =>
+            {
+                var win = new ThreadWindow();                 
+                win.ShowDialog();
+            }));
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = true;
+            thread.Start();
 
-		async public Task StartNewThread()
-		{
-			Dispatcher externaldisp = null;
-			var thread = new Thread(new ThreadStart(() =>
-			{
-				var win = new ThreadWindow();
-				externaldisp = Dispatcher.CurrentDispatcher;
-				win.ShowDialog();
-			}));
-			thread.SetApartmentState(ApartmentState.STA);
-			thread.IsBackground = true;
-			thread.Start();
+            await Task.Delay(1000); //give time to open the above window
 
-			await Task.Delay(1000); //give time to open the above window
-
-			//the below statement works fine if the above thread did not start
-			//Failed
-			await externaldisp.BeginInvoke(DispatcherPriority.Send, new Action(async () => await instance.ShowMessageAsync(this, "Test", "Message")));
-			
-			//Failed
-			await instance.ShowMessageAsync(this, "Test", "Message");
-
-		}
-	}
+            //the below statement works fine if the above thread did not start
+            //Failed 
+          await  mydisp.BeginInvoke(new Action(async()=> { await instance.ShowMessageAsync(this, "Test", "Message"); }), DispatcherPriority.Send);
+        }
+    }
 }
