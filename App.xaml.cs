a7ba6486cebbec369a9c1d02fb5fd14acd4cb006
Moustafa Khalil <mostafa90@gmail.com>
2020-01-14 09:23:21 +0400

    cleanup

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,26 +1,14 @@
-﻿using LiveCharts.Core;
-using LiveCharts.Core.Coordinates;
-using LiveCharts.Core.Interaction.Series;
-using LiveCharts.Core.Themes;
-using LiveCharts.Wpf;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Test_Repo
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App: Application
     {
         public App()
         {
-           
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,100 +1,77 @@
 ﻿using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Test_Repo
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : MetroWindow
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow: MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
 
-			var uc_base = new uc_BaseControl();
-			// MainGrid.Children.Add(uc_base);
-			// Grid.SetRow(uc_base, 1);
+            var uc_base = new uc_BaseControl();
+            // MainGrid.Children.Add(uc_base);
+            // Grid.SetRow(uc_base, 1);
 
-			Content = uc_base;
-			PreviewMouseDoubleClick += delegate
-			 {
-				 uc_base.flout.IsOpen = !uc_base.flout.IsOpen;
-			 };
+            Content = uc_base;
+            PreviewMouseDoubleClick += delegate
+             {
+                 uc_base.flout.IsOpen = !uc_base.flout.IsOpen;
+             };
 
-			DataContext = new mc_test(); ;
+            DataContext = new mc_test(); ;
+        }
 
+        async private void Test_Click(object sender, RoutedEventArgs e)
+        {
+            var prg = await this.ShowProgressAsync($"Window Size Mode is:  {this.SizeToContent.ToString()}", "", true);
+            prg.Canceled += async delegate
+            {
+                await prg.CloseAsync();
+            };
+        }
+    }
 
+    public class mc_test: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
 
-		}
+        public mc_test()
+        {
+            Items = new ObservableCollection<string>();
+            Items.Add("123");
+            Items.Add("ddas");
+            Items.Add("asda");
+            Items.Add("12adas3");
+        }
 
+        #region Items
 
+        private ObservableCollection<string> _Items;
 
-		async private void Test_Click(object sender, RoutedEventArgs e)
-		{
-			var prg = await this.ShowProgressAsync($"Window Size Mode is:  {this.SizeToContent.ToString()}", "", true);
-			prg.Canceled += async delegate
-			{
-				await prg.CloseAsync();
-			};
-		}
-	}
+        public ObservableCollection<string> Items
+        {
+            get
+            {
+                return _Items;
+            }
+            set { _Items = value; Notify(nameof(Items)); }
+        }
 
-	public class mc_test : INotifyPropertyChanged
-	{
-		public event PropertyChangedEventHandler PropertyChanged;
+        #endregion Items
 
-
-		public mc_test()
-		{
-			Items = new ObservableCollection<string>();
-			Items.Add("123");
-			Items.Add("ddas");
-			Items.Add("asda");
-			Items.Add("12adas3");
-		}
-		#region Items
-
-		private ObservableCollection<string> _Items;
-
-		public ObservableCollection<string> Items
-		{
-			get
-			{
-				return _Items;
-			}
-			set { _Items = value; Notify(nameof(Items)); }
-
-		}
-		#endregion
-
-
-		public void Notify(string prop)
-		{
-			if (PropertyChanged != null)
-			{
-
-
-				PropertyChanged(this, new PropertyChangedEventArgs(prop));
-			}
-		}
-	}
+        public void Notify(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
 }
